18f8952e2c4becfacc291fbf3aca32a1cc350fc7
VD <megadiak@gmail.com>
2023-07-18 08:47:01 +0300

    Added Hosre tests

---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,75 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class HorseTest {
+    @Test
+    public void constructorNullFirst() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse(null, 2.3, 50));
+    }
+
+    @Test
+    public void constructorNullFirstThrowMassage() {
+        Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse(null, 2.3, 50));
+        Assertions.assertEquals("Name cannot be null.", throwable.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "   "})
+    public void constructorSpacesFirst (String str) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse(str, 2.3, 50));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "   "})
+    public void constructorSpacesFirstThrowMassage (String str) {
+        Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse(str, 2.3, 50));
+        Assertions.assertEquals("Name cannot be blank.", throwable.getMessage());
+    }
+
+    @Test
+    public void constructorNegativeSecond() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse("vfvf", -2.3, 50));
+    }
+
+    @Test
+    public void constructorNegativeSecondThrowMassage() {
+        Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse("dvfd", -2.3, 50));
+        Assertions.assertEquals("Speed cannot be negative.", throwable.getMessage());
+    }
+
+    @Test
+    public void constructorNegativeThird() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse("vfvf", 2.3, -50));
+    }
+
+    @Test
+    public void constructorNegativeThirdThrowMassage() {
+        Throwable throwable = Assertions.assertThrows(IllegalArgumentException.class, () -> new Horse("dvfd", 2.3, -50));
+        Assertions.assertEquals("Distance cannot be negative.", throwable.getMessage());
+    }
+
+    @Test
+    public void getNameReturnNameFromConstructor() {
+        Horse horse = new Horse("bla", 2.3, 50);
+        Assertions.assertEquals("bla", horse.getName());
+    }
+
+    @Test
+    public void getSpeedReturnSpeedFromConstructor() {
+        Horse horse = new Horse("bla", 2.3, 50);
+        Assertions.assertEquals(2.3, horse.getSpeed());
+    }
+
+    @Test
+    public void getDistanceReturnDistanceFromConstructor() {
+        Horse horse = new Horse("bla", 2.3, 50);
+        Assertions.assertEquals(50, horse.getDistance());
+    }
+
+    @Test
+    public void getDistanceReturnZeroFromSecondConstructor() {
+        Horse horse = new Horse("bla", 2.3);
+        Assertions.assertEquals(0, horse.getDistance());
+    }
+}
